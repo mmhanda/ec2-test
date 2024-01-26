b26b524e8e959edf25eb3511c1c378922dd47fb7
mhanda <mohammedhanda109@gmail.com>
2024-01-26 05:43:31 +0100

    crawlee

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-const startUrls = ['https://1337.ma/en/'];
+const startUrls = ['https://www.thecollector.com/'];
 
 app.get('/test', async (req, res) => {
   /////////////////////////////////////////////
