aeacaffd2b3e66455a4f455b202a0b7f0bdbdb7e
mhanda <mohammedhanda109@gmail.com>
2024-01-26 03:08:22 +0100

    crawlee

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-import express from "express";
+// For more information, see https://crawlee.dev/
+import { CheerioCrawler, Configuration } from '@crawlee/cheerio';
+import express from 'express';
 
 const app = express();
+app.use(express.json());
 
-app.listen(5001, () => { console.log("App running...") });
+const startUrls = ['https://1337.ma/en/'];
 
-app.get('/', (req, res) => { res.json("My api running") });
+app.get('/test', async (req, res) => {
+  /////////////////////////////////////////////
+  const crawler = new CheerioCrawler({
+    // maxRequestsPerCrawl: 5,
+    async requestHandler({ $, request, enqueueLinks, pushData }) {
+      const title = $('title').text();
+      console.log(`The title of "${request.url}" is: ${title}.`);
+      const Content = await $('body').text();
+      await pushData({ title, url: request.loadedUrl, Content });
+      await enqueueLinks();
+
+      // await enqueueLinks({
+      //     strategy: 'same-domain'
+      // }); // this to add subdomains
+    }
+  }, new Configuration({
+    persistStorage: false,
+  }));
+
+  await crawler.run(startUrls);
+
+  const data = await crawler.getData();
+
+  return res.send(JSON.stringify(data));
+  // res.send("HI");
+  /////////////////////
+  // return {
+  //   statusCode: 200,
+  // body: await crawler.getData(),
+  // }
+});
+
+app.listen(5001, () => console.log("listening on 5001"));
